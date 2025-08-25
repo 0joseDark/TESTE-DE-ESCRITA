75593a4ab87d00f8ef9759203c7707a65035c784
jose <162716366+0joseDark@users.noreply.github.com>
2025-08-25 12:50:23 +0100

    Create 0001-Ola-Mundo-qt.py

---
+++ 0001-Ola-Mundo-qt.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class OlaMundoJanela(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Título da janela
+        self.setWindowTitle("Exemplo Qt - Olá Mundo")
+
+        # Mensagem inicial
+        self.label = QLabel("Clique no botão para dizer Olá!", self)
+
+        # Botão
+        self.botao = QPushButton("Dizer Olá", self)
+        self.botao.clicked.connect(self.mostrar_mensagem)
+
+        # Botão para sair
+        self.botao_sair = QPushButton("Sair", self)
+        self.botao_sair.clicked.connect(self.close)
+
+        # Layout (organização dos widgets na janela)
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.botao)
+        layout.addWidget(self.botao_sair)
+
+        self.setLayout(layout)
+
+    def mostrar_mensagem(self):
+        self.label.setText("Olá, mundo!")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    janela = OlaMundoJanela()
+    janela.show()
+    sys.exit(app.exec_())
